e1f2900c90f5aa32590f90f86f3d69fafe08e97e
Shahad  Sidek <73951870+shahadsidek@users.noreply.github.com>
2023-02-20 19:09:50 +0300

    responsiveness done

---
+++ new/script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", function () {
+  navbar.classList.toggle("responsive")
+  
+}
+)
