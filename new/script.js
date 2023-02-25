c9f3d0402258b53039049d76a6e20eba61486cda
Shahad  Sidek <73951870+shahadsidek@users.noreply.github.com>
2023-02-25 19:40:15 +0300

    fixed links

--- new/script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu");
-
-menuIcon.addEventListener("click", function () {
-  navbar.classList.toggle("responsive")
-  
-}
-)
